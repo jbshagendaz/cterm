e04378c14aee03da43e5f418c4b4014e7e37f518
jbshagendaz <nicholas.matsuura@gmail.com>
2015-04-04 11:23:14 -0400

    loop in

--- src/cterm/core.clj
+++ src/cterm/core.clj
@@ -1,6 +1,14 @@
-(ns cterm.core)
+(ns cterm.core
+  (use [clojure.java.shell :only [sh]]))
 
 (defn -main 
-  "I don't do a whole lot."
   []
-  (println "Hello, World!"))
+   (println "Enter text:")
+    
+   (loop  [input  (read-line)]
+     (when-not  (= ":done" input)
+       (println  (str "You entered: >>" input "<<"))
+       (recur  (read-line))))
+    
+   (println "End")
+   )
