ab2118e98b638d9ecd5b277ef3321e5d4ee9bbee
jbshagendaz <nicholas.matsuura@gmail.com>
2015-04-04 10:23:28 -0400

    window works

--- src/cterm/core.clj
+++ src/cterm/core.clj
@@ -1,13 +1,11 @@
 (ns cterm.core
   (use [clojure.java.shell :only [sh]]))
-;(require '[lanterna.terminal :as terminal])
+  (require '[lanterna.terminal :as terminal])
 
 (defn -main 
-  "I don't do a whole lot."
+  "" 
   []
-  ;(def term (terminal/get-terminal :swing))
-  ;(terminal/start term)
-  ;(while (pos? 2) 
-  ;(terminal/put-character term (terminal/get-key-blocking term)))
-  
-  (sh "ls" "-a"))
+  (def term (terminal/get-terminal :swing))
+  (terminal/start term)
+  (while (pos? 2) 
+    (terminal/put-character term (terminal/get-key-blocking term))))
