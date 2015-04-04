70cb2267ed97065ef247041dda199230184a4308
Nicholas Matsuura <jb@jb-mbp.local>
2015-04-04 09:54:49 -0400

    n

--- src/cterm/core.clj
+++ src/cterm/core.clj
@@ -1,6 +1,13 @@
-(ns cterm.core)
+(ns cterm.core
+  (use [clojure.java.shell :only [sh]]))
+;(require '[lanterna.terminal :as terminal])
 
 (defn -main 
   "I don't do a whole lot."
   []
-  (println "Hello, World!"))
+  ;(def term (terminal/get-terminal :swing))
+  ;(terminal/start term)
+  ;(while (pos? 2) 
+  ;(terminal/put-character term (terminal/get-key-blocking term)))
+  
+  (sh "ls" "-a"))
