3d11013eb3f1190f53b907c1e7769e98f669cb03
Nicholas Matsuura <jb@wireless1x-168-122-15-148.bu.edu>
2015-04-14 20:26:21 -0400

    Finished basic cd (absolute and relative) Still need to implement "cd .."

--- src/cterm/core.clj
+++ src/cterm/core.clj
@@ -5,41 +5,52 @@
                               trim-newline]])
   (require [clj-commons-exec :as exec]))
 
+(def last-dir (:out @(exec/sh ["pwd"])))
+(def curr-dir (:out @(exec/sh ["pwd"])))
 
 (defn get-home []
   (trim-newline (:out @(exec/sh ["printenv" "HOME"]))))
 
-(defn replace-home [directory]
-  (trim-newline (replace-first directory (get-home) "~")))
+(defn replace-home [dir]
+  (trim-newline (replace-first dir (get-home) "~")))
 
-(defn print-prompt [directory]
-  (printf "\n%s\t%s" (replace-home directory) (:out @(exec/sh ["date" "+%X"]))) 
+(defn print-prompt []
+  (printf "\n%s\t%s" (replace-home curr-dir) (:out @(exec/sh ["date" "+%X"]))) 
   (printf "$ ")
   (flush))
 
 (defn process-input [input] 
   (let [input-vector (split input #"\s+")
-        pipe?       (boolean (re-find #"\|" input))]
-
+        pipe?        (boolean (re-find #"\|" input))
+        cd?          (= "cd" (first input-vector))]
     {:input-vector input-vector
      
      ;cd
-     :dir (when (= "cd" (first input-vector)) (second input-vector))
+     :cd? cd?
+     :rel?  (when cd? (not= \/ (get (second input-vector) 0)))
+     :abs?  (when cd? (= \/ (get (second input-vector) 0)))
     
      ;pipes 
      :pipe? pipe?   
      :pipe-input-1 (when pipe? (split (trim (first  (split input #"\|"))) #"\s+"))
      :pipe-input-2 (when pipe? (split (trim (second (split input #"\|"))) #"\s+")) 
      
-     ;history
-
      ;new process 
      :process? ()}))
 
-(defn execute-input [input] 
+(defn execute-input [input]
+  (def last-dir curr-dir) 
+  (when (input :rel?) (def curr-dir (str curr-dir "/" (second (input :input-vector)))))
+  (when (input :abs?) (def curr-dir (second (input :input-vector))))
+  
   (cond
-    (input :pipe?) @(last (exec/sh-pipe (input :pipe-input-1) (input :pipe-input-2)))
-    :else @(exec/sh (input :input-vector))))
+    (input :cd?) (let [output-map @(exec/sh ["cd" curr-dir])]
+                   (when (not (nil? (:err output-map))) 
+                     (do (def curr-dir last-dir)))
+                   output-map)
+
+    (input :pipe?) @(last (exec/sh-pipe (input :pipe-input-1) (input :pipe-input-2) {:dir curr-dir}))
+    :else @(exec/sh (input :input-vector) {:dir curr-dir})))
 
 (defn print-output [output-map]
   (when (not (zero? (output-map :exit))) (printf "error code: %s\n" (output-map :exit)))
@@ -54,7 +65,7 @@
 (defn -main 
   []
   (def history [])
-  (print-prompt (get @(exec/sh ["pwd"]) :out)) 
+  (print-prompt) 
   (doseq  
     [line (take-while (partial not= "quit") (repeatedly read-line))]
      (cond
@@ -64,6 +75,5 @@
              (execute-input)
              (print-output)))
 
-     ;(println (process-input line))
      (def history (conj history (vector line)))  
-     (print-prompt (get @(exec/sh ["pwd"]) :out))))
+     (print-prompt)))
